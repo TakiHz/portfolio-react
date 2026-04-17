f7feeab1615f76cc65016e00a4a81ceca1cf2830
Mizael <talix080@gmail.com>
2026-04-17 20:12:24 -0300

    Update home.js

--- home.js
+++ home.js
@@ -4,6 +4,7 @@ function Home() {
   return (
     <section id="home" className="hero">
       <div className="hero-content">
+        <img src="https://drive.google.com/uc?export=view&id=12tzbq_aahSc6SZcrMhUzQ_ToImk--l5N" alt="Foto de Mizael Ferreira" className="foto-perfil" />
         <p className="saudacao">Olá, eu sou</p>
         <h1>Mizael Ferreira</h1>
         <p className="descricao">
